de9516e419b0be764d3ce83fba65da341f7293ff
jeetwide <wide.angle456@gmail.com>
2020-06-26 19:41:43 +0530

    first commit

---
+++ client/src/context/GlobalState.js
@@ -0,0 +1,79 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+import axios from "axios";
+
+const initialState = {
+  transactions: [],
+  error: null,
+  loading: true,
+};
+
+//creating context
+export const GlobalContext = createContext(initialState);
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  //Actions
+
+  async function getTransactions() {
+    try {
+      const res = await axios.get("/api/v1/transactions");
+
+      dispatch({
+        type: "GET_TRANSACTIONS",
+        payload: res.data.data,
+      });
+    } catch (err) {
+      console.log("Error in GET ", err);
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: err.response.data.error,
+      });
+    }
+  }
+  async function deleteTransaction(id) {
+    try {
+      await axios.delete(`/api/v1/transactions/${id}`);
+      dispatch({
+        type: "DELETE_TRANSACTION",
+        payload: id,
+      });
+    } catch (err) {}
+  }
+
+  async function addTransaction(transaction) {
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+    try {
+      const res = await axios.post("/api/v1/transactions", transaction, config);
+      dispatch({
+        type: "ADD_TRANSACTION",
+        payload: transaction,
+      });
+    } catch (err) {
+      dispatch({
+        type: 'TRANSACTION_ERROR',
+        payload: err.response.data.error
+      });
+    }
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        error: state.error,
+        loading: state.loading,
+        getTransactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
